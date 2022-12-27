c030e4749ecd8de30b31311cd0dcbf95cff1f9f6
abhay8696 <abhaykamble1346@gmail.com>
2022-12-27 15:58:19 +0530

    completed styles for country details page

--- src/components/detailPage.js
+++ src/components/detailPage.js
@@ -21,7 +21,7 @@ const DetailPage = props => {
         if(!array) return null;
         let arr = [];
         array.forEach(i => {
-            arr.push(<span>{i}</span>)
+            arr.push(<span className='detailPageButton borderButton'>{i}</span>)
         });
         return arr;
     }
@@ -29,33 +29,58 @@ const DetailPage = props => {
     return (
         <div className={themeForPage()}>
             <div className='detailPageMenu'>
-                <div onClick={()=>toggleDetailPageOFF()}>
+                <div onClick={()=>toggleDetailPageOFF()} className='detailPageButton'>
                     <BsArrowLeft/> <span>Back</span>
                 </div>
             </div>
             <div className='detailPageContent'>
-                <div className='bigFlag'>
-                    <img src={country?.flags?.png} />
-                </div>
+                <img src={country?.flags?.png} />
                 <div className='countryDetails'>
-                    <div className='countryDetails-name'>
+                    <h1 className='countryDetails-name'>
                         {country?.name?.common}
+                    </h1>
+                    <div className='countryDetails-main'>
+                        <div className='countryDetails-a'>
+                            <span className='detailPair'>
+                                <span className='detailKey'>Native Name:</span>
+                                <span className='detailValue'>{Object.values(country?.name?.nativeName)?.[0]?.common}</span>
+                            </span>
+                            <span className='detailPair'>
+                                <span className='detailKey'>Population:</span> 
+                                <span className='detailValue'>{country?.population}</span>
+                            </span>
+                            <span className='detailPair'>
+                                <span className='detailKey'>Region:</span> 
+                                <span className='detailValue'>{country?.region}</span>
+                            </span>
+                            <span className='detailPair'>
+                                <span className='detailKey'>Sub-region:</span> 
+                                <span className='detailValue'>{country?.subregion}</span>
+                            </span>
+                            <span className='detailPair'>
+                                <span className='detailKey'>Capital:</span> 
+                                <span className='detailValue'>{country?.capital?.[0]}</span>
+                            </span>
+                        </div>
+                        <div className='countryDetails-b'>
+                            <span className='detailPair'>
+                                <span className='detailKey'>Top Level Domain:</span>
+                                <span className='detailValue'>{country?.tld?.[0]}</span>
+                            </span>
+                            <span className='detailPair'>
+                                <span className='detailKey'>Currency:</span> 
+                                <span className='detailValue'>{Object.values(country?.currencies)?.[0]?.name}</span>
+                            </span>
+                            
+                            <span className='detailPair'>
+                                <span className='detailKey'>Languages:</span> 
+                                <span className='detailValue'>{Object.values(country?.languages)?.[0]}</span>
+                            </span>
+                        </div>
                     </div>
-                    <div className='countryDetails-a'>
-                        <span>Native Name: {Object.values(country?.name?.nativeName)?.[0]?.common}</span>
-                        <span>Population: {country?.population}</span>
-                        <span>Region: {country?.region}</span>
-                        <span>Sub-region: {country?.subregion}</span>
-                        <span>Capital: {country?.capital?.[0]}</span>
-                    </div>
-                    <div className='countryDetails-b'>
-                        <span>Top Level Domain: {country?.tld?.[0]}</span>
-                        <span>Currency: {Object.values(country?.currencies)?.[0]?.name}</span>
-                        
-                        <span>Languages: {Object.values(country?.languages)?.[0]}</span>
-                    </div>
-                    <div>
-                        Borders: <span>{displayBorders(country?.borders)}</span>
+                    <div className='bordersDiv'>
+                        <span className='detailKey'>Borders:</span> 
+                        <span className='borderButtonsDiv'>{displayBorders(country?.borders)}</span>
                     </div>
                 </div>
             </div>
